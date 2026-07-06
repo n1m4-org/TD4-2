35fe64c453cebf8563c583fe29228e32c743a95e
kitajima-hayato <k023g0057@g.neec.ac.jp>
2026-07-06 23:59:02 +0900

    ボムエネミーのリフレクト後の処理の実装 まだ反射はできていません

--- application/gameobject/component/action/enemy/bomb/BombMoveComponent.cpp
+++ application/gameobject/component/action/enemy/bomb/BombMoveComponent.cpp
@@ -13,15 +13,21 @@ GameObjectComponent::BombMoveComponent::BombMoveComponent(GameObject* player)
 
 void GameObjectComponent::BombMoveComponent::Update(GameObject* owner)
 {
+#pragma region 変数の初期化
 	// プレイヤーの情報がない場合は処理を中断
-	if (player_ == nullptr)return;
-	
+	if (player_ == nullptr)
+		return;
+
 	// 爆発済みなら停止
 	if (hasExploded_)
 	{
 		physics_->SetMovementVelocity({0.0f, 0.0f, 0.0f});
 		return;
 	}
+
+	// リフレクト後の寿命を初期化
+	reflectedLifespan_ = lifespan_;
+#pragma endregion 変数の初期化
 
 	// フィジクスコンポーネントの取得
 	physics_ = owner->GetComponent<PhysicsComponent>().get();
@@ -37,37 +43,60 @@ void GameObjectComponent::BombMoveComponent::Update(GameObject* owner)
 	float distance = toPlayer.Length();
 
 
-// 突進開始判定（まだ突進していない & 突進距離に入った）
-	if (!isDashing_ && distance <= dashRange_)
+	// 索敵範囲内に入ったら追尾開始（＝点火）
+	if (!isDashing_ && distance <= chaseRange_)
 	{
 		isDashing_ = true;
 	}
 
 	if (isDashing_)
 	{
-		// 突進中のみライフスパンを消費
+		// 点火中はライフスパンを消費
 		--lifespan_;
 
 		if (lifespan_ <= 0.0f)
 		{
-			// 時間切れで爆発（1回だけ）
+			// 寿命切れで爆発（1回だけ）
 			physics_->SetMovementVelocity({0.0f, 0.0f, 0.0f});
 			ParticleManager::GetInstance()->Play("bomber", owner->GetPosition());
 			hasExploded_ = true;
 			return;
 		}
 
-		// 突進中も毎フレーム方向を更新して追尾し続ける
-		physics_->SetMovementVelocity(toPlayer.Normalize() * dashSpeed_);
-	}
-	else if (distance <= chaseRange_)
-	{
-		// 索敵範囲内：追尾
+		// 点火中も毎フレーム方向を更新して追尾し続ける
 		physics_->SetMovementVelocity(toPlayer.Normalize() * chaseSpeed_);
 	}
-	else
+	// 索敵範囲外で突進中でない場合は待機
+	else if (!isDashing_ && !isReflected_)
 	{
-		// 範囲外：待機
+		// 索敵範囲外：待機
 		physics_->SetMovementVelocity({0.0f, 0.0f, 0.0f});
+	}
+
+	// ここに反射されたらフラグを立てるコードを追加
+	// isReflected_ = 
+
+	// プレイヤーからの反射情報を取得する
+	// reflectedDirection_ = 
+	// reflectedVelocity_ =
+
+	// リフレクト後の処理
+	if (isReflected_)
+	{
+		// 移動不可
+		isDashing_ = false;
+
+		// ライフスパンを消費
+		--reflectedLifespan_;
+
+		if (reflectedLifespan_ <= 0.0f)
+		{
+			// 寿命切れで爆発
+			physics_->SetMovementVelocity({0.0f, 0.0f, 0.0f});
+			ParticleManager::GetInstance()->Play("bomber", owner->GetPosition());
+			hasExploded_ = true;
+			return;
+		}
+
 	}
 }
