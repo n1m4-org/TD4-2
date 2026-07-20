c126df35621c2dffb528d28956e2cf4887af4ced
kitajima-hayato <k023g0057@g.neec.ac.jp>
2026-07-20 16:37:14 +0900

    ダッシュとか

---
+++ application/gameobject/component/action/enemy/dash/DashMoveComponent.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "gameobject/component/base/IActionComponent.h"
+#include "math/Vector3.h"
+
+namespace GameObjectComponent
+{
+
+	class DashMoveComponent : public IActionComponent
+	{
+		GameObject* player_ = nullptr;
+
+		float elapsedTime_ = 0.f;
+		float triggerTime_ = 3.f;
+
+		Vector3 dashDirection_ = {};
+		float speed_ = 125.f;
+
+		enum class State { Idle, Dash };
+		State state_ = State::Idle;
+
+		float currentSpd_ = 0.f;
+		const float Decay = 0.05f;
+		const float MinSpd = 0.5f;
+
+		float BrakeCoefficient = 600.f;
+
+	public:
+		DashMoveComponent(GameObject* _player);
+		void Update(GameObject* _owner) override;
+
+	private:
+		void Idle(const GameObject* _owner);
+		void Dash(const GameObject* _owner);
+		void Debug();
+	};
+
+}
