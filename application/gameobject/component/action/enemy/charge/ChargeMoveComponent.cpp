a2603044995414a4a8445e08159ed1952a707ce3
iiokaisami <146907095+iiokaisami@users.noreply.github.com>
2026-07-20 16:17:03 +0900

    Feature charge enemy (#28)
    * developマージ
    
    * 弾初期化コンポ作成途中
    
    * 弾の初期化用の作った
    
    * chore: auto-organize filters [skip ci]
    
    * 左右移動をMoveに移行
    
    * 左右移動できた
    
    * いらない処理消した
    
    * chore: auto-organize filters [skip ci]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- application/gameobject/component/action/enemy/charge/ChargeMoveComponent.cpp
+++ application/gameobject/component/action/enemy/charge/ChargeMoveComponent.cpp
@@ -105,7 +105,9 @@ void GameObjectComponent::ChargeMoveComponent::Charge(GameObject* owner)
 			state_ = State::Fire;
 		}
 		// 確認用回転させる
-		owner->SetRotation(owner->GetRotation() + Vector3{0.0f, 1.0f, 0.0f});
+		Vector3 rotation = owner->GetRotation();
+		rotation.y += rotationSpeed_ * TimeManager::GetInstance().GetGameContext().deltaTime;
+		owner->SetRotation(rotation);
 	}
 }
 
@@ -114,14 +116,15 @@ void GameObjectComponent::ChargeMoveComponent::Cooldown(GameObject* owner)
 	// クールタイム進行
 	coolTime_ += TimeManager::GetInstance().GetGameContext().deltaTime;
 
+	StrafeMove(owner);
+
 	// クールタイムが一定時間を超えたら攻撃状態を解除
 	if (coolTime_ >= kCoolTime)
 	{
 		isAttacking_ = false;
 		coolTime_ = 0.0f;
 
-		// 確認用のサイズを元に戻す
-		owner->SetScale(Vector3{2.0f, 2.0f, 2.0f});
+		
 		state_ = State::Move;
 	}
 }
@@ -205,4 +208,45 @@ void GameObjectComponent::ChargeMoveComponent::BulletInitialize(GameObject* owne
 		collider->SetOnStay([](const CollisionInfo&) {});
 		collider->SetOnExit([](const CollisionInfo&) {});
 	}
+}
+
+void GameObjectComponent::ChargeMoveComponent::StrafeMove(GameObject* owner)
+{
+	// タイマー更新
+	strafeTimer_ += TimeManager::GetInstance().GetGameContext().deltaTime;
+
+	// 確認用のサイズを元に戻す
+	owner->SetScale(Vector3{2.0f, 2.0f, 2.0f});
+
+	if (strafeTimer_ >= changeTime_)
+	{
+		moveRight_ = Random(0, 1) > 0.3f;
+
+		changeTime_ = Random(1.0f, 2.5f);
+		currentStrafeSpeed_ = Random(4.5f, 6.5f);
+
+		strafeTimer_ = 0.0f;
+	}
+
+	// プレイヤーへの方向
+	Vector3 toPlayer = player_->GetPosition() - owner->GetPosition();
+	float distance = toPlayer.Length();
+	toPlayer.NormalizeSelf();
+
+	// 横方向
+	Vector3 side = {-toPlayer.z, 0.0f, toPlayer.x};
+
+	// 左右移動
+	if (!moveRight_)
+	{
+		side *= -1.0f;
+	}
+
+	owner->SetRotation(Vector3{0.0f, atan2f(toPlayer.x, toPlayer.z), 0.0f});
+	owner->SetPosition(owner->GetPosition() + side * currentStrafeSpeed_ * TimeManager::GetInstance().GetGameContext().deltaTime);
+}
+
+float GameObjectComponent::ChargeMoveComponent::Random(float min, float max)
+{
+	return min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min)));
 }
