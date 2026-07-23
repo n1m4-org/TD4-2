7a855d2bbc974339ab08c3928388502105f42182
iiokaisami <k023g0025@g.neec.ac.jp>
2026-07-23 20:08:13 +0900

    タイトルだけBGM入れてるほかSEはどの処理かわからん

--- application/scene/play/title/state/TitleWaitState.cpp
+++ application/scene/play/title/state/TitleWaitState.cpp
@@ -1,9 +1,13 @@
 #include "TitleWaitState.h"
 #include "engine/scene/interface/BaseScene.h"
 #include "input/Input.h"
+#include "audio/Audio.h"
 
 void TitleWaitState::OnEnter(BaseScene& scene)
 {
+	Audio::GetInstance()->PlayWave("TitleBGM", true);
+	// BGMの音量を調整（0.0f～1.0f）
+	Audio::GetInstance()->SetVolume("TitleBGM", 0.5f);
 }
 
 void TitleWaitState::OnUpdate(BaseScene& scene)
